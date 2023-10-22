a2d33e7523c6b7cd7a4a4ce975daa9138b5c3ef5
adityaraj1114 <adityarajmdb1114@gmail.com>
2023-10-22 16:12:51 +0530

    to do list

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addTask() {
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(m){
+    if(m.target.tagName === "LI"){
+        m.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(m.target.tagName === "SPAN") {
+        m.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
